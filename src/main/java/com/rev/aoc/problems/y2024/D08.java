d7c280a378799eccd7c09ec15bdf905864cd3130
RevsRev <eddie.revell@yahoo.co.uk>
2024-12-08 06:27:11 +0000

    Added solution to day 8

---
+++ src/main/java/com/rev/aoc/problems/y2024/D08.java
@@ -0,0 +1,115 @@
+package com.rev.aoc.problems.y2024;
+
+import com.rev.aoc.AocCoordinate;
+import com.rev.aoc.problems.AocProblem;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public final class D08 extends AocProblem {
+
+    public static final char EMPTY_CHAR = '.';
+
+    @Override
+    public AocCoordinate getCoordinate() {
+        return new AocCoordinate(2024, 8);
+    }
+
+    @Override
+    protected long partOneImpl() {
+        char[][] grid = loadResourcesAsCharArray();
+        int height = grid.length;
+        int width = grid[0].length;
+
+        Map<Character, Set<Pair<Integer, Integer>>> antennas = loadAntennas(grid);
+        Set<Pair<Integer, Integer>> antinodes = new HashSet<>();
+        for (Map.Entry<Character, Set<Pair<Integer, Integer>>> entry : antennas.entrySet()) {
+            Set<Pair<Integer, Integer>> antennasForChar = entry.getValue();
+            List<Pair<Integer, Integer>> antennasForCharList = antennasForChar.stream().toList();
+            addAntinodesForAntennas(height, width, antinodes, antennasForCharList, false);
+        }
+        return antinodes.size();
+    }
+
+    @Override
+    protected long partTwoImpl() {
+        char[][] grid = loadResourcesAsCharArray();
+        int height = grid.length;
+        int width = grid[0].length;
+
+        Map<Character, Set<Pair<Integer, Integer>>> antennas = loadAntennas(grid);
+        Set<Pair<Integer, Integer>> antinodes = new HashSet<>();
+        for (Map.Entry<Character, Set<Pair<Integer, Integer>>> entry : antennas.entrySet()) {
+            Set<Pair<Integer, Integer>> antennasForChar = entry.getValue();
+            List<Pair<Integer, Integer>> antennasForCharList = antennasForChar.stream().toList();
+            addAntinodesForAntennas(height, width, antinodes, antennasForCharList, true);
+        }
+        return antinodes.size();
+    }
+
+    private void addAntinodesForAntennas(final int height,
+                                         final int width,
+                                         final Set<Pair<Integer, Integer>> antinodes,
+                                         final List<Pair<Integer, Integer>> antennasForCharList,
+                                         final boolean partTwo) {
+        for (int i = 0; i < antennasForCharList.size(); i++) {
+            for (int j = i + 1; j < antennasForCharList.size(); j++) {
+                Pair<Integer, Integer> top = antennasForCharList.get(i);
+                Pair<Integer, Integer> bottom = antennasForCharList.get(j);
+                Pair<Integer, Integer> diff =
+                        Pair.of(bottom.getLeft() - top.getLeft(), bottom.getRight() - top.getRight());
+
+                int iterations = 0;
+                while (inBounds(height, width, top)) {
+                    if (!partTwo && iterations == 1 && inBounds(height, width, top)) {
+                        antinodes.add(top);
+                    } else if (partTwo) {
+                        if (inBounds(height, width, top)) {
+                            antinodes.add(top);
+                        }
+                    }
+                    top = Pair.of(top.getLeft() - diff.getLeft(), top.getRight() - diff.getRight());
+                    iterations++;
+                }
+
+                iterations = 0;
+                while (inBounds(height, width, bottom)) {
+                    if (!partTwo && iterations == 1 && inBounds(height, width, bottom)) {
+                        antinodes.add(bottom);
+                    } else if (partTwo) {
+                        if (inBounds(height, width, bottom)) {
+                            antinodes.add(bottom);
+                        }
+                    }
+                    bottom = Pair.of(bottom.getLeft() + diff.getLeft(), bottom.getRight() + diff.getRight());
+                    iterations++;
+                }
+            }
+        }
+    }
+
+    private boolean inBounds(int height, int width, final Pair<Integer, Integer> candidate) {
+        int i = candidate.getLeft();
+        int j = candidate.getRight();
+        return i >= 0 && i < height && j >= 0 && j < width;
+    }
+
+    private Map<Character, Set<Pair<Integer, Integer>>> loadAntennas(final char[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+
+        Map<Character, Set<Pair<Integer, Integer>>> antennas = new HashMap<>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i][j] != EMPTY_CHAR) {
+                    antennas.computeIfAbsent(grid[i][j], k -> new HashSet<>()).add(Pair.of(i, j));
+                }
+            }
+        }
+        return antennas;
+    }
+}
