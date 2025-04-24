1e65b4434920a6089bb02ac9de6967871d2b1d53
Eddie Revell <eddie.revell@yahoo.co.uk>
2025-04-25 00:00:03 +0100

    Addd solution to 2015:02

---
+++ src/main/java/com/rev/aoc/problems/y2015/D02.java
@@ -0,0 +1,53 @@
+package com.rev.aoc.problems.y2015;
+
+import com.rev.aoc.framework.problem.AocCoordinate;
+import com.rev.aoc.framework.problem.AocProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class D02 extends AocProblem<Long, Long> {
+
+
+    @Override
+    @SuppressWarnings("checkstyle:MagicNumber")
+    public AocCoordinate getCoordinate() {
+        return new AocCoordinate(2015, 2);
+    }
+
+    @Override
+    protected Long partOneImpl() {
+        List<Box> boxes = parseBoxes(loadResources());
+        long areaNeeded = 0;
+        for (Box b : boxes) {
+            long area = 2 * (b.l * b.w + b.l * b.h + b.w * b.h);
+            long slack = Math.min(b.l * b.w, Math.min(b.l * b.h, b.w * b.h));
+            areaNeeded += (area + slack);
+        }
+        return areaNeeded;
+    }
+
+    @Override
+    protected Long partTwoImpl() {
+        List<Box> boxes = parseBoxes(loadResources());
+        long ribbonNeeded = 0;
+        for (Box b : boxes) {
+            long volume = b.l * b.w * b.h;
+            long smallestPerimeter = 2 * Math.min(b.l + b.w, Math.min(b.l + b.h, b.w + b.h));
+            ribbonNeeded += (volume + smallestPerimeter);
+        }
+        return ribbonNeeded;
+    }
+
+    private List<Box> parseBoxes(final List<String> inputs) {
+        final List<Box> boxes = new ArrayList<>();
+        for (String s : inputs) {
+            String[] lwh = s.split("x");
+            boxes.add(new Box(Integer.parseInt(lwh[0]), Integer.parseInt(lwh[1]), Integer.parseInt(lwh[2])));
+        }
+        return boxes;
+    }
+
+    private record Box(long l, long w, long h) {
+    }
+}
