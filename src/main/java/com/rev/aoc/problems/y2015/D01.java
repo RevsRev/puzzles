765b41f334db2f82d1454cc1fb5065953c694dc3
Eddie Revell <eddie.revell@yahoo.co.uk>
2025-04-24 23:48:42 +0100

    Added 2015:01

---
+++ src/main/java/com/rev/aoc/problems/y2015/D01.java
@@ -0,0 +1,49 @@
+package com.rev.aoc.problems.y2015;
+
+import com.rev.aoc.framework.problem.AocCoordinate;
+import com.rev.aoc.framework.problem.AocProblem;
+import com.rev.aoc.framework.problem.ProblemExecutionException;
+
+public final class D01 extends AocProblem<Long, Long> {
+
+    private static final char UP = '(';
+    private static final char DOWN = ')';
+    public static final int BASEMENT = -1;
+
+    @Override
+    @SuppressWarnings("checkstyle:MagicNumber")
+    public AocCoordinate getCoordinate() {
+        return new AocCoordinate(2015, 1);
+    }
+
+    @Override
+    protected Long partOneImpl() {
+        String input = loadResources().get(0);
+        long floor = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == UP) {
+                floor++;
+            } else {
+                floor--;
+            }
+        }
+        return floor;
+    }
+
+    @Override
+    protected Long partTwoImpl() {
+        String input = loadResources().get(0);
+        long floor = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == UP) {
+                floor++;
+            } else {
+                floor--;
+            }
+            if (floor == BASEMENT) {
+                return (long) i + 1;
+            }
+        }
+        throw new ProblemExecutionException();
+    }
+}
