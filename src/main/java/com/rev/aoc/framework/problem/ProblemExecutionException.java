d83c589b919a793e57a8819d667e788390bc6cb5
Eddie Revell <eddie.revell@yahoo.co.uk>
2025-04-24 23:49:19 +0100

    Added new constructor to ProblemExecutionException

--- src/main/java/com/rev/aoc/framework/problem/ProblemExecutionException.java
+++ src/main/java/com/rev/aoc/framework/problem/ProblemExecutionException.java
@@ -2,6 +2,10 @@ package com.rev.aoc.framework.problem;
 
 public final class ProblemExecutionException extends RuntimeException {
 
+    public ProblemExecutionException() {
+        super();
+    }
+
     public ProblemExecutionException(final String message, final Throwable cause) {
         super(message, cause);
     }
