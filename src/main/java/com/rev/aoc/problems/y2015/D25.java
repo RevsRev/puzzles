52798416540c991fa2ce92a4acbd6415626b6914
Eddie Revell <eddie.revell@yahoo.co.uk>
2025-05-05 23:38:29 +0100

    Added solution to 2015:25

---
+++ src/main/java/com/rev/aoc/problems/y2015/D25.java
@@ -0,0 +1,38 @@
+package com.rev.aoc.problems.y2015;
+
+import com.rev.aoc.framework.problem.AocCoordinate;
+import com.rev.aoc.framework.problem.AocProblem;
+
+public final class D25 extends AocProblem<Long, Long> {
+
+
+    public static final int STARTING_CODE = 20151125;
+    public static final int CODE_MULT = 252533;
+    public static final int CODE_MOD = 33554393;
+    public static final int PROBLEM_ROW = 3010;
+    public static final int PROBLEM_COLUMN = 3019;
+
+    @Override
+    @SuppressWarnings("checkstyle:MagicNumber")
+    public AocCoordinate getCoordinate() {
+        return new AocCoordinate(2015, 25);
+    }
+
+    @Override
+    protected Long partOneImpl() {
+        final long triangleBase = (long) PROBLEM_ROW + (long) PROBLEM_COLUMN - 1;
+        final long triangleNumber = (triangleBase - 1) * (triangleBase) / 2;
+        final long codeSequence = triangleNumber + (long) PROBLEM_COLUMN - 1;
+
+        long code = STARTING_CODE;
+        for (int i = 0; i < codeSequence; i++) {
+            code = (code * CODE_MULT) % CODE_MOD;
+        }
+        return code;
+    }
+
+    @Override
+    protected Long partTwoImpl() {
+        return null;
+    }
+}
