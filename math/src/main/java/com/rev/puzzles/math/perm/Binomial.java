bf6178b1a4b175d03d169df247913f2bce13e1c1
RevsRev <eddie.revell@yahoo.co.uk>
2025-09-27 18:47:25 +0100

    Add computation of binomial coefficients to math util

---
+++ math/src/main/java/com/rev/puzzles/math/perm/Binomial.java
@@ -0,0 +1,51 @@
+package com.rev.puzzles.math.perm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Binomial {
+
+    private static final List<List<Long>> PASCALS_TRIANGLE = new ArrayList<>();
+
+    private Binomial() {
+    }
+
+    /**
+     * Compute the value of n choose m.
+     *
+     * We could use the formula n!/(m!(n-m)!), however this is trickier to use as it involves computing
+     * large factorials (or implementing some clever cancelling of factors).
+     *
+     * Instead, we take the approach of caching pascals triangle, which is O(n^2) for
+     * a triangle of height n. In most problems, this will suffice.
+     * @param n
+     * @param m
+     * @return n choose m
+     */
+    public static long nCm(final int n, final int m) {
+        computeNthRow(n);
+        return PASCALS_TRIANGLE.get(n).get(m);
+    }
+
+    private static void computeNthRow(final int n) {
+        if (PASCALS_TRIANGLE.size() > n) {
+            return;
+        }
+
+        if (PASCALS_TRIANGLE.isEmpty()) {
+            PASCALS_TRIANGLE.add(List.of(1L));
+        }
+
+        while (PASCALS_TRIANGLE.size() <= n) {
+            final List<Long> lastRow = PASCALS_TRIANGLE.get(PASCALS_TRIANGLE.size() - 1);
+            final List<Long> nextRow = new ArrayList<>(lastRow.size() + 1);
+            nextRow.add(1L);
+            for (int i = 0; i < lastRow.size() - 1; i++) {
+                nextRow.add(lastRow.get(i) + lastRow.get(i + 1));
+            }
+            nextRow.add(1L);
+            PASCALS_TRIANGLE.add(nextRow);
+        }
+    }
+
+}
---
+++ math/src/test/java/com/rev/puzzles/math/perm/BinomialTest.java
@@ -0,0 +1,18 @@
+package com.rev.puzzles.math.perm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinomialTest {
+
+    @Test
+    public void testBinomialCoefficientsAreCorrect() {
+        assertEquals(1, Binomial.nCm(0 ,0));
+        assertEquals(1, Binomial.nCm(1 ,0));
+        assertEquals(1, Binomial.nCm(1 ,1));
+        assertEquals(2, Binomial.nCm(2 ,1));
+        assertEquals(6, Binomial.nCm(4 ,2));
+    }
+
+}
