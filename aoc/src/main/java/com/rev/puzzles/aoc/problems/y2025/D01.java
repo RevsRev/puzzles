cc06e0821ab364a4517dec377c915d45dd13f9aa
RevsRev <eddie.revell@yahoo.co.uk>
2025-12-01 07:54:46 +0000

    Make aoc 2025:1:2 a bit faster

--- aoc/src/main/java/com/rev/puzzles/aoc/problems/y2025/D01.java
+++ aoc/src/main/java/com/rev/puzzles/aoc/problems/y2025/D01.java
@@ -30,29 +30,28 @@ public final class D01 {
         int start = 50;
         int count = 0;
         for (final Integer i : dialsList) {
-            if (i >= 0) {
-                for (int j = 1; j <= i; j++) {
-                    start += 1;
-                    start = (start + 100) % 100;
-                    if (start == 0) {
-                        count++;
-                    }
-                }
-            } else {
-                for (int j = -1; j >= i; j--) {
-                    start -= 1;
-                    start = (start + 100) % 100;
-                    if (start == 0) {
-                        count++;
-                    }
+            final int remainder = Math.abs(i) % 100;
+            final int completeRotations = Math.abs(i) / 100;
+
+            if (start != 0 && remainder != 0) {
+                if (i < 0 && start - remainder <= 0) {
+                    count++;
+                } else if (i > 0 && start + remainder >= 100) {
+                    count++;
                 }
             }
+
+            count += completeRotations;
+
+            start += i % 100;
+            start = (start + 100) % 100;
+
         }
         return count;
     }
 
     private static List<Integer> getDialMovements(final ProblemResourceLoader<List<String>> resourceLoader) {
-        final List<Integer> dialsList = resourceLoader.resources().stream().map(s -> {
+        return resourceLoader.resources().stream().map(s -> {
             if (s.contains("L")) {
                 s = s.replace("L", "");
                 return -1 * Integer.parseInt(s);
@@ -60,6 +59,5 @@ public final class D01 {
                 return Integer.parseInt(s.replace("R", ""));
             }
         }).toList();
-        return dialsList;
     }
 }
