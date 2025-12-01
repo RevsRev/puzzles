56c583731c789921dcf1df9217a1dbc50ee1bd9b
RevsRev <eddie.revell@yahoo.co.uk>
2025-12-01 07:54:52 +0000

    Tweak aoc.sh script

--- aoc.sh
+++ aoc.sh
@@ -1,3 +1,3 @@
-mvn clean package -Dcheckstyle.skip -DskipTests
+mvn package -Dcheckstyle.skip -DskipTests
 
 java -jar app/target/app-1.0-SNAPSHOT-jar-with-dependencies.jar --engine aoc --problem $1
