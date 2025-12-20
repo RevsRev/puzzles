9d06dec48112a06368c88a62edd74537c67f2479
RevsRev <eddie.revell@yahoo.co.uk>
2025-12-20 23:12:48 +0000

    Add more tests to grid polygon

--- math/src/test/java/com/rev/puzzles/math/geom/GridPolygonTest.java
+++ math/src/test/java/com/rev/puzzles/math/geom/GridPolygonTest.java
@@ -80,4 +80,70 @@ class GridPolygonTest {
                 lPolygon.sides
         );
     }
+
+    @Test
+    void shouldCreaterShape() {
+        final GridPolygon lPolygon = GridPolygon.createFromGridSquareCorners(
+                List.of(
+                        new GridPoint(0, 0),
+                        new GridPoint(0, 6),
+                        new GridPoint(4, 6)
+                )
+        );
+        Assertions.assertEquals(
+                List.of(
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 0), new GridPoint(0, 7)), LEFT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 7), new GridPoint(5, 7)), UP),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(5, 7), new GridPoint(5, 6)), RIGHT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(5, 6), new GridPoint(1, 6)), DOWN),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(1, 6), new GridPoint(1, 0)), RIGHT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(1, 0), new GridPoint(0, 0)), DOWN)
+                ),
+                lPolygon.sides
+        );
+    }
+
+    @Test
+    void shouldCreate7Shape() {
+        final GridPolygon lPolygon = GridPolygon.createFromGridSquareCorners(
+                List.of(
+                        new GridPoint(0, 0),
+                        new GridPoint(0, 4),
+                        new GridPoint(-10, 4)
+                )
+        );
+        Assertions.assertEquals(
+                List.of(
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 0), new GridPoint(0, 4)), LEFT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 4), new GridPoint(-10, 4)), DOWN),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-10, 4), new GridPoint(-10, 5)), LEFT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-10, 5), new GridPoint(1, 5)), UP),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(1, 5), new GridPoint(1, 0)), RIGHT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(1, 0), new GridPoint(0, 0)), DOWN)
+                ),
+                lPolygon.sides
+        );
+    }
+
+    @Test
+    void shouldCreateBackwardsLShape() {
+        final GridPolygon lPolygon = GridPolygon.createFromGridSquareCorners(
+                List.of(
+                        new GridPoint(-6, 0),
+                        new GridPoint(-1, 0),
+                        new GridPoint(-1, 3)
+                )
+        );
+        Assertions.assertEquals(
+                List.of(
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-6, 1), new GridPoint(-1, 1)), UP),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-1, 1), new GridPoint(-1, 4)), LEFT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-1, 4), new GridPoint(0, 4)), UP),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 4), new GridPoint(0, 0)), RIGHT),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(0, 0), new GridPoint(-6, 0)), DOWN),
+                        new GridPolygon.PolygonSide(GridSide.create(new GridPoint(-6, 0), new GridPoint(-6, 1)), LEFT)
+                ),
+                lPolygon.sides
+        );
+    }
 }
