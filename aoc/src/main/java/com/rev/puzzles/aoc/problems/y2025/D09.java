a3c402dbdfaca57521771e0d54885c0547ec0b46
RevsRev <eddie.revell@yahoo.co.uk>
2025-12-21 15:10:44 +0000

    Add solution to 2025 day 9

--- aoc/src/main/java/com/rev/puzzles/aoc/problems/y2025/D09.java
+++ aoc/src/main/java/com/rev/puzzles/aoc/problems/y2025/D09.java
@@ -1,11 +1,21 @@
 package com.rev.puzzles.aoc.problems.y2025;
 
 import com.rev.puzzles.aoc.framework.AocProblemI;
+import com.rev.puzzles.aoc.framework.AocVisualisation;
 import com.rev.puzzles.framework.framework.ProblemResourceLoader;
 import com.rev.puzzles.math.geom.GridPoint;
 import com.rev.puzzles.math.geom.GridPolygon;
 import com.rev.puzzles.math.geom.GridPolygonBuilder;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,7 +45,6 @@ public final class D09
     @AocProblemI(year = 2025, day = 9, part = 2)
     public long partTwoImpl(final ProblemResourceLoader<List<String>> resourceLoader)
     {
-
         final List<GridPoint> redTiles = getGridPoints(resourceLoader);
 
         final List<GridPoint> loopedList = new ArrayList<>(redTiles);
@@ -44,19 +53,78 @@ public final class D09
         final GridPolygon polygon = GridPolygonBuilder.createFromGridSquareCorners(loopedList);
 
         long maxArea = 0;
+        for (int i = 0; i < loopedList.size(); i++)
+        {
+            for (int j = i; j < loopedList.size(); j++)
+            {
+                final GridPolygon rectangle = GridPolygonBuilder.rectangle(loopedList.get(i), loopedList.get(j), true);
+                final long area = rectangle.area();
+                if (area > maxArea && rectangle.isInteriorOf(polygon)) {
+                    maxArea = area;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    @AocVisualisation(year = 2025, day = 9, part = 2)
+    public void visualise(final ProblemResourceLoader<List<String>> resourceLoader) {
+        final List<GridPoint> redTiles = getGridPoints(resourceLoader);
+
+        final List<GridPoint> loopedList = new ArrayList<>(redTiles);
+        loopedList.add(redTiles.get(0));
+
+        final GridPolygon polygon = GridPolygonBuilder.createFromGridSquareCorners(loopedList);
+
+        long maxArea = 0;
+        GridPolygon maxRectangle = null;
 
         for (int i = 0; i < loopedList.size(); i++)
         {
             for (int j = i; j < loopedList.size(); j++)
             {
                 final GridPolygon rectangle = GridPolygonBuilder.rectangle(loopedList.get(i), loopedList.get(j), true);
-                if (rectangle.isInteriorOf(polygon)) {
-                    maxArea = Math.max(maxArea, rectangle.area());
+                final long area = rectangle.area();
+                if (area > maxArea && rectangle.isInteriorOf(polygon)) {
+                    maxArea = area;
+                    maxRectangle = rectangle;
                 }
             }
         }
 
-        return maxArea;
+
+        final XYSeries polygonSeries = getXySeries(polygon, "polygon");
+        final XYSeries rectangleSeries = getXySeries(maxRectangle, "rectangle");
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(rectangleSeries);
+        dataset.addSeries(polygonSeries);
+
+        final JFreeChart xyLineChart = ChartFactory.createXYLineChart(
+                "Polygon",
+                "X",
+                "Y",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = xyLineChart.getXYPlot();
+        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
+        renderer.setSeriesLinesVisible(0, true);
+        renderer.setSeriesLinesVisible(1, true);
+
+        renderer.setSeriesVisible(0, true);
+        renderer.setSeriesPaint(0, Color.BLUE);
+        renderer.setSeriesVisible(1, true);
+        renderer.setSeriesPaint(1, Color.RED);
+
+        final ChartFrame frame = new ChartFrame("Plot", xyLineChart);
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private static List<GridPoint> getGridPoints(ProblemResourceLoader<List<String>> resourceLoader) {
@@ -70,5 +138,14 @@ public final class D09
                 }
         ).toList();
         return redTiles;
+    }
+
+    private static XYSeries getXySeries(final GridPolygon shape, final String name) {
+        final XYSeries polygonSeries = new XYSeries(name, false, true);
+        for (GridPolygon.PolygonSide side : shape.getSides()) {
+            polygonSeries.addOrUpdate(side.side().start().x(), side.side().start().y());
+            polygonSeries.addOrUpdate(side.side().end().x(), side.side().end().y());
+        }
+        return polygonSeries;
     }
 }
